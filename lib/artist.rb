3991f13b7b0720c0e9dd3fac1720d067fc372ebe
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-01 01:00:35 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,28 @@
-class Artist 
-  attr_accessor :name, :songs
-  
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-  
-  def songs
-    @songs
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def self.song_count
-    @@song_count
-  end
-end 
+class Artist
+    attr_accessor :name
+    
+    @@song_count = 0
+    
+    def initialize (name)
+        @name = name
+        @songs = []
+    end
+    def songs
+        Song.all
+    end
+    def add_song (song)
+        @songs << song
+        song.artist = self
+        @@song_count += 1
+    end
+    def add_song_by_name (name)
+        song = Song.new(name)
+        # @songs << song
+        add_song(song)
+        song.artist = self
+        @@song_count += 1
+    end
+    def self.song_count
+        @@song_count
+    end
+end
