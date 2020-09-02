3ed0f287f477e9a17f48afb759874a3113be242e
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-02 04:19:22 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,27 @@
 class Author 
-  attr_accessor :name
+  attr_accessor :name, :posts
   @@post_count = 0
-
+  
   def initialize(name)
     @name = name
     @posts = []
   end
-
+  
   def posts
-    Post.all
-  end
-
+    Post.all 
+  end 
+  
   def add_post(post)
     @posts << post
     post.author = self
     @@post_count += 1
   end
 
-  def add_post_by_title(name)
-    post = Post.new(name)
-    #@posts << post
+  def add_post_by_title(title)
+    post = Post.new(title)
+   # @posts << post
     post.author = self
-    @@post_count += 1
+    @@post_count += 2
   end
 
   def self.post_count
